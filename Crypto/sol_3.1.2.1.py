e78478ddc24bd74526d7baa712d8bcab80bc4942
mungsoo <346582379@qq.com>
2019-03-04 11:24:38 -0600

    mp3

---
+++ Crypto/sol_3.1.2.1.py
@@ -0,0 +1,25 @@
+import sys
+
+if len(sys.argv) != 4:
+    print("Error: Invalid number of arguments")
+    sys.exit()
+ciphertext_file, key_file, output_file = sys.argv[1], sys.argv[2], sys.argv[3]
+
+with open(ciphertext_file) as f:
+    ciphertext = f.read().strip()
+with open(key_file) as f:
+    key = f.read().strip()
+    
+# print(ciphertext)
+# print(key)
+
+result = []
+for i, v in enumerate(ciphertext):
+    if v.isalpha():
+        result.append(key[ord(v) - ord('A')])
+    else:
+        result.append(v)
+
+with open(output_file, 'w') as f:
+    f.write("".join(result))
+# print("".join(result))
